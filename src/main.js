15d017e513917a7396dc13e018b18ebe62342de5
puge <mail@puge.org>
2020-06-12 19:37:13 +0800

    save

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ function scrollIntoView () {
 }
 
 // 阻止微信拖动
-document.body.addEventListener('touchmove', function (e) {
-  e.preventDefault() // 阻止默认的处理方式(阻止下拉滑动的效果)
-}, {passive: false})
+// document.body.addEventListener('touchmove', function (e) {
+//   e.preventDefault() // 阻止默认的处理方式(阻止下拉滑动的效果)
+// }, {passive: false})
+
+// 微信加载完毕自动播放音乐
+document.addEventListener("WeixinJSBridgeReady", function () {
+  // @|bg.mp3|
+  var music = new Audio("./static/resource/bg.mp3")
+  music.loop = true
+  music.play()
+}, false)
